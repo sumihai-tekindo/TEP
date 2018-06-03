0fcdde8a85ed9f402c7555728258a0563f55d30c
Joenan Nadhir Rayhan <joenan@Joenans-MacBook.local>
2018-06-04 06:14:35 +0700

    remove ".pyc.orig"

---
+++ account_analytic_budget_revision/models/account.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Nicolas Bessi
+#    Copyright 2011-2012 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from odoo import api, fields, models, _
+
+
+class AccountAnalyticAccount(models.Model):
+    _inherit = 'account.analytic.account'
+    
+    @api.multi
+    def _compute_practical_amount(self):
+        for analytic in self:
+            result = 0.0
+            self.env.cr.execute("""
+                SELECT SUM(balance)
+                FROM account_move_line
+                WHERE analytic_account_id=%s""",
+            (analytic.id,))
+            result = self.env.cr.fetchone()[0] or 0.0
+            analytic.practical_amount = -result
+            analytic.balance_amount = analytic.planned_amount + result
+    
+    @api.multi
+    def _compute_theoritical_amount(self):
+        today = fields.Datetime.now()
+        for line in self:
+            # Used for the report
+
+            if self.env.context.get('wizard_date_from') and self.env.context.get('wizard_date_to'):
+                date_from = fields.Datetime.from_string(self.env.context.get('wizard_date_from'))
+                date_to = fields.Datetime.from_string(self.env.context.get('wizard_date_to'))
+                if date_from < fields.Datetime.from_string(line.date_from):
+                    date_from = fields.Datetime.from_string(line.date_from)
+                elif date_from > fields.Datetime.from_string(line.date_to):
+                    date_from = False
+
+                if date_to > fields.Datetime.from_string(line.date_to):
+                    date_to = fields.Datetime.from_string(line.date_to)
+                elif date_to < fields.Datetime.from_string(line.date_from):
+                    date_to = False
+
+                theo_amt = 0.00
+                if date_from and date_to:
+                    line_timedelta = fields.Datetime.from_string(line.date_to) - fields.Datetime.from_string(line.date_from)
+                    elapsed_timedelta = date_to - date_from
+                    if elapsed_timedelta.days > 0:
+                        theo_amt = (elapsed_timedelta.total_seconds() / line_timedelta.total_seconds()) * line.planned_amount
+            else:
+                line_timedelta = fields.Datetime.from_string(line.date_to) - fields.Datetime.from_string(line.date_from)
+                elapsed_timedelta = fields.Datetime.from_string(today) - (fields.Datetime.from_string(line.date_from))
+
+                if elapsed_timedelta.days < 0:
+                    # If the budget line has not started yet, theoretical amount should be zero
+                    theo_amt = 0.00
+                elif line_timedelta.days > 0 and fields.Datetime.from_string(today) < fields.Datetime.from_string(line.date_to):
+                    # If today is between the budget line date_from and date_to
+                    theo_amt = (elapsed_timedelta.total_seconds() / line_timedelta.total_seconds()) * line.planned_amount
+                else:
+                    theo_amt = line.planned_amount
+
+            line.theoritical_amount = theo_amt
+
+    @api.multi
+    def _compute_percentage(self):
+        for line in self:
+            if line.theoritical_amount != 0.00:
+                line.percentage = float((line.practical_amount or 0.0) / line.theoritical_amount) * 100
+            else:
+                line.percentage = 0.00
+    
+    date_from = fields.Date('Start Date')
+    date_to = fields.Date('End Date')
+    planned_amount = fields.Float('Planned Amount', required=True, digits=0)
+    practical_amount = fields.Float(compute='_compute_practical_amount', string='Actual Amount', digits=0)
+    balance_amount = fields.Float(compute='_compute_practical_amount', string='Balance Amount', digits=0)
+    level_3_id = fields.Many2one('budget.level.three', string="Level 3", required=True)
+    level_2_id = fields.Many2one('budget.level.two', string="Level 2", related="level_3_id.level_2_id", readonly=True, store=True)
+    level_1_id = fields.Many2one('budget.level.one', string="Level 1", related="level_3_id.level_2_id.level_1_id", readonly=True, store=True)
+#     theoritical_amount = fields.Float(compute='_compute_theoritical_amount', string='Theoretical Amount', digits=0)
+#     percentage = fields.Float(compute='_compute_percentage', string='Achievement')
+
+
+class BudgetLevelOne(models.Model):
+    _name = 'budget.level.one'
+    _description = 'Budget Level 1'
+
+    name = fields.Char(string='Name', required=True)
+
+
+class BudgetLevelTwo(models.Model):
+    _name = 'budget.level.two'
+    _description = 'Budget Level 2'
+
+    name = fields.Char(string='Name', required=True)
+    level_1_id = fields.Many2one('budget.level.one', string="Level 1")
+
+
+class BudgetLevelThree(models.Model):
+    _name = 'budget.level.three'
+    _description = 'Budget Level 3'
+
+    name = fields.Char(string='Name', required=True)
+    level_2_id = fields.Many2one('budget.level.two', string="Level 2")
--- purchase_spk/models/opname.py
+++ purchase_spk/models/opname.py
@@ -13,15 +13,17 @@ class OpnameMandor(models.Model):
     @api.depends('opname_line.price_total')
     def _amount_all(self):
         for opname in self:
-            amount_untaxed = amount_tax = 0.0
+            amount_total = 0.0
             for line in opname.opname_line:
-                amount_total += line.price_subtotal
+                amount_total += line.price_total
             opname.update({
                 'amount_total': opname.currency_id.round(amount_total),
             })
             
     READONLY_STATES = {
         'open': [('readonly', True)],
+        'qs_approve': [('readonly', True)],
+        'pm_approve': [('readonly', True)],
         'done': [('readonly', True)],
         'cancel': [('readonly', True)],
     }
@@ -33,20 +35,20 @@ class OpnameMandor(models.Model):
     project_id = fields.Many2one('project.project', string='Project', required=True, states=READONLY_STATES)
     task_id = fields.Many2one('project.task', string='Task', required=True, states=READONLY_STATES)
     date = fields.Date(string='Order Date', required=True, states=READONLY_STATES)
-    sequence = fields.Date(string='Opname Sequence', readonly=True)
+    sequence = fields.Integer(string='Opname Sequence', readonly=True)
+    period_start = fields.Date(string='Period', required=True, states=READONLY_STATES)
+    period_end = fields.Date(string='Period End', required=True, states=READONLY_STATES)
     opname_line = fields.One2many('opname.mandor.line', 'opname_id', string='Lines', states=READONLY_STATES)
     
     company_id = fields.Many2one('res.company', 'Company', required=True, index=True, default=lambda self: self.env.user.company_id.id)
     currency_id = fields.Many2one('res.currency', 'Currency', required=True, default=lambda self: self.env.user.company_id.currency_id.id)
     user_id = fields.Many2one('res.users', string='Done by', default=lambda self: self.env.uid, required=True, states=READONLY_STATES)
 
-    amount_untaxed = fields.Monetary(string='Untaxed Amount', store=True, readonly=True, compute='_amount_all', track_visibility='always')
-    amount_tax = fields.Monetary(string='Taxes', store=True, readonly=True, compute='_amount_all')
     amount_total = fields.Monetary(string='Total', store=True, readonly=True, compute='_amount_all')
     
     state = fields.Selection([('draft', 'Draft'),
+                              ('qs_approve', 'QS Approval'),
                               ('pm_approve', 'PM Approval'),
-                              ('dir_approve', 'Director Approval'),
                               ('open', 'In Progress'),
                               ('done', 'Done'),
                               ('cancel', 'Cancelled')], string='State', default='draft', readonly=True, copy=False, index=True)
@@ -55,15 +57,15 @@ class OpnameMandor(models.Model):
     @api.multi
     def action_confirm(self):
         for opname in self:
-            opname.state = 'pm_approve' if opname.amount_total <= opname.company_id.pmdir_treshhold_amount else 'dir_approve'
+            opname.state = 'qs_approve'
+    
+    @api.multi
+    def action_qs_approve(self):
+        for opname in self:
+            opname.state = 'pm_approve'
     
     @api.multi
     def action_pm_approve(self):
-        for opname in self:
-            opname.state = 'open'
-    
-    @api.multi
-    def action_dir_approve(self):
         for opname in self:
             opname.state = 'open'
     
@@ -93,34 +95,64 @@ class OpnameMandor(models.Model):
             self.task_id = False
         return {'domain': {'task_id': [('project_id', '=', self.project_id.id)]}}
     
+    @api.onchange('spk_id')
+    def onchange_spk_id(self):
+        self.opname_line = [(5, 0)]
+        if not self.spk_id:
+            return {}
+        spk = self.spk_id
+        line_vals = []
+        for line in self.spk_id.order_line:
+            line_vals.append((0, 0, {
+                'name': line.name,
+                'uom_id': line.uom_id.id,
+                'account_analytic_id': line.account_analytic_id.id,
+                'qty_spk': line.quantity,
+                'price_spk': line.price_unit,
+                'price_total_spk': line.price_subtotal,
+            }))
+        return {'value': {
+            'partner_id': spk.partner_id.id,
+            'project_id': spk.project_id.id,
+            'task_id': spk.task_id.id,
+            'opname_line': line_vals,
+        }}
+    
     
 class OpnameMandorLine(models.Model):
     _name = 'opname.mandor.line'
     _description = 'Opname Lines'
 
-    @api.depends('quantity', 'price_unit', 'taxes_id')
+    @api.depends('qty')
     def _compute_amount(self):
         for line in self:
-            taxes = line.taxes_id.compute_all(line.price_unit, line.opname_id.currency_id, line.quantity, product=False, partner=line.opname_id.partner_id)
             line.update({
-                'price_tax': taxes['total_included'] - taxes['total_excluded'],
-                'price_total': taxes['total_included'],
-                'price_subtotal': taxes['total_excluded'],
+                'price_total': line.qty,
             })
             
     sequence = fields.Integer(string='Sequence', default=10)
     name = fields.Char(string='Description', required=True)
     opname_id = fields.Many2one('opname.mandor', string='Opname')
     state = fields.Selection(related='opname_id.state', store=True)
-    product_id = fields.Many2one('product.product', string='Product', required=True)
-    account_analytic_id = fields.Many2one('account.analytic.account', string='Analytic Account', required=True)
-    quantity = fields.Float(string='Volume', digits=dp.get_precision('Product Unit of Measure'), required=True)
-    uom_id = fields.Many2one('product.uom', string='UoM', required=True)
-    price_unit = fields.Float(string='Unit Price', digits=dp.get_precision('Product Price'), required=True)
-    taxes_id = fields.Many2many('account.tax', string='Taxes', domain=[('active', '=', True)])
     company_id = fields.Many2one('res.company', 'Company', related='opname_id.company_id', readonly=True)
     currency_id = fields.Many2one('res.currency', 'Currency', related='opname_id.currency_id', readonly=True)
+#     product_id = fields.Many2one('product.product', string='Product', required=True)
+    uom_id = fields.Many2one('product.uom', string='UoM', required=True)
+    account_analytic_id = fields.Many2one('account.analytic.account', string='Analytic Account', required=True)
     
-    price_subtotal = fields.Monetary(compute='_compute_amount', string='Subtotal', store=True)
+    qty_spk = fields.Float(string='Vol SPK', digits=dp.get_precision('Qty'), readonly=True)
+    price_spk = fields.Float(string='Price SPK', digits=dp.get_precision('Product Price'), readonly=True)
+    price_total_spk = fields.Monetary(string='Total', readonly=True)
+    
+    amount_ret_release = fields.Monetary(string='Ret Release Amount')
+    
+    ret_hold = fields.Boolean(string='Hold Ret')
+    
+    qty = fields.Float(string='Volume', digits=dp.get_precision('Product Unit of Measure'), required=True)
     price_total = fields.Monetary(compute='_compute_amount', string='Total', store=True)
+    amount_ret = fields.Monetary(string='Ret Amount')
+    amount_net = fields.Monetary(string='Net Amount')
+    qty_balance = fields.Float(string='Balance Vol', digits=dp.get_precision('Product Unit of Measure'), required=True)
+    amount_ret_exc = fields.Monetary(string='Exc Ret Amount')
+    
     price_tax = fields.Monetary(compute='_compute_amount', string='Tax', store=True)
